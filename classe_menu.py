85d787335575b770aa7cd25887f0908632302fce
jackson1002 <102551009+jackson1002@users.noreply.github.com>
2023-01-25 11:45:14 -0300

    classe_menu

--- classe_menu.py
+++ classe_menu.py
@@ -1,4 +1,4 @@
-path = 'C:\\Users\\20211174010006\\Desktop\\teste03\\cardapio.txt'
+path =  'C:\\Users\\20211174010006\\Documents\\GitHub\\teste-restaurante\\cardapio.txt'
 try:
     with open(path, 'r') as f:
         estoque = f.readlines()
@@ -8,12 +8,13 @@ except:
 
 class Produto:
     def __init__(self):
+        self.estoque = ''
         print('Deseja colocar produtos no seu estoque? ')
         resposta = int(input(' Digite: 1 para SIM e 2 para NÃO: '))
         if resposta == 1:
             r = 0
             while  r != 4 :
-                self.codigo = str(input('codigo: '))
+                self.codigo = int(input('codigo: '))
                 self.nomeProduto = str(input('nome do seu produto: '))
                 self.precoProduto = str(input('coloque o preço: '))
                 r = int(input('deseja adicionar outro produto? 3 para SIM 4 para NÃO > '))
@@ -21,27 +22,54 @@ class Produto:
                     f.write(f'{self.codigo} :: {self.nomeProduto} :: R${self.precoProduto}\n')
                 with open(path, 'r') as f:
                     self.estoque = f.readlines()
-        print('você saiu da opção de adicionar produtos no estoque.')
-        print(self.estoque)
+                    
+        else:
+            print('você saiu da opção de adicionar produtos no estoque.')
+    
 
 class Menu(Produto):
     def __init__(self):
+        pass
+    
+    def Organiza_Produtos(self):
         self.nome_menu = input('Por favor informe o tipo do menu: ')
+        with open(path, 'a') as f:
+            f.write('\n')
         with open(path,'a') as f:
             f.write('{}\n'.format(self.nome_menu))
-    
-    def Add_Produto(self):
         
-         
+        if self.nome_menu == 'Salgados':
+            for c in estoque:
+                if 'hamburguer' in c:
+                    self.salgados = []
+                    self.salgados.append(c)
+                    with open(path, 'a') as f:
+                        f.write('{}'.format(c))
+                    break
         
-    
+        elif self.nome_menu == 'Bebidas':
+            for c in estoque:
+                if 'refrigerante' in c or 'suco' in c:
+                    self.bebidas = []
+                    self.bebidas.append(c)
+                    with open(path, 'a') as f:
+                        f.write('{}'.format(c))
+                    break
 
-
+        '''
+        else:
+            for c in self.estoque:
+                self.doces = []
+                self.doces.append(self.estoque)
         
+        print('Salgados: {}'.format(self.salgados)) 
+        print('Bebidas: {}'.format(self.bebidas))
+        
+        print('Doces: {}'.format(self.doces))
+        '''
 
+cliente = Produto()
 
-
-
-
-produtos = Produto()
+cliente = Menu()
+cliente.Organiza_Produtos()
 

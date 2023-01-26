1b02bd9a10699f4889b02bbcc9a54514819d38e9
Emilly Estefanny <emilly.e@escolar.ifrn.edu.br>
2023-01-26 15:32:38 -0300

    atualizando

--- emilly/main.py
+++ emilly/main.py
@@ -2,36 +2,27 @@ from produto import Produto
 from menu import Menu
 
 
+
 while True:
-    opcao = int(input('''DIGITE UMA OPÇÃO -
-1 - ENTRAR NO MENU
-2 - ADICIONAR PRODUTO
-3 - SAIR DO PROGRAMA
-> '''))
+    opcao = int(input('DIGITE UMA OPÇÃO\n1 - ENTRAR NO MENU\n2 - ADICIONAR PRODUTO\n3 - SAIR DO PROGRAMA\n > '))
     if opcao == 1:
         Menu()
+
     elif opcao == 2:
         resposta2 = 0
         while  resposta2 != 4 :
-            tipoProduto = int(input('''Tipo produto:
-            1 - Pratos
-            2 - Bebidas
-            3 - Sobremesas '''))
+            print('1 - Pratos\n2 - Bebidas\n3 - Sobremesas')
+            tipoProduto = int(input('> '))
             codigo = str(input('Codigo: '))
             nomeProduto = str(input('Nome do seu produto: '))
             precoProduto = str(input('Coloque o preço: '))
+
             if tipoProduto == 1:
-                
-                
-                
+                with open('pratos.txt' 'a') as f:
+                    f.write(f'{codigo} :: {nomeProduto} :: {precoProduto} ::\n')
             
-
-                
-
-
-
             resposta2 = int(input('Deseja adicionar outro produto?\n SIM: 3 NÃO: 4 > '))
-
             produto = Produto(codigo, nomeProduto, precoProduto)
+
     elif opcao == 3:
         break
